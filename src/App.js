550a5058bb97ddfaec9039869e5dbbb08c437771
aianov <122548754+aianov@users.noreply.github.com>
2023-03-05 20:55:38 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -24,17 +24,17 @@ function App() {
   return (
     <Router>
       <div className="sketch">
-        <Ring1 speed={0.3} sz1={450} sz2={450} xcoor={400} ycoor={400}/>
-        <Ring2 speed={0.2} sz1={500} sz2={500} xcoor={400} ycoor={400}/>
-        <Ring3 speed={0.1} sz1={550} sz2={550} xcoor={400} ycoor={400}/>
-        <Ring4 speed={0.2} sz1={700} sz2={700} xcoor={700} ycoor={700}/>
-        <Ring5 speed={0.3} sz1={750} sz2={750} xcoor={750} ycoor={750}/>
-        <Ring6 speed={0.2} sz1={900} sz2={900} xcoor={900} ycoor={900}/>
-        <Ring7 speed={0.1} sz1={950} sz2={950} xcoor={950} ycoor={950}/>
-        <Ring8 speed={0.2} sz1={1100} sz2={1100} xcoor={1100} ycoor={1100}/>
-        <Ring9 speed={0.2} sz1={1300} sz2={1300} xcoor={1300} ycoor={1300}/>
-        <Ring10 speed={0.3} sz1={1350} sz2={1350} xcoor={1350} ycoor={1350}/>
-        <Ring11 speed={0.1} sz1={1450} sz2={1450} xcoor={1450} ycoor={1450}/>
+        <Ring1 speed={0.23} sz1={450} sz2={450} xcoor={400} ycoor={400}/>
+        <Ring2 speed={-0.15} sz1={500} sz2={500} xcoor={400} ycoor={400}/>
+        <Ring3 speed={0.10} sz1={550} sz2={550} xcoor={400} ycoor={400}/>
+        <Ring4 speed={-0.15} sz1={700} sz2={700} xcoor={700} ycoor={700}/>
+        <Ring5 speed={-0.16} sz1={750} sz2={750} xcoor={750} ycoor={750}/>
+        <Ring6 speed={0.10} sz1={900} sz2={900} xcoor={900} ycoor={900}/>
+        <Ring7 speed={0.05} sz1={950} sz2={950} xcoor={950} ycoor={950}/>
+        <Ring8 speed={-0.17} sz1={1100} sz2={1100} xcoor={1100} ycoor={1100}/>
+        <Ring9 speed={0.15} sz1={1300} sz2={1300} xcoor={1300} ycoor={1300}/>
+        <Ring10 speed={0.2} sz1={1350} sz2={1350} xcoor={1350} ycoor={1350}/>
+        <Ring11 speed={-0.15} sz1={1400} sz2={1400} xcoor={1450} ycoor={1450}/>
         <StarAnimation />
         <div className="star1"></div>
         <div className="star2"></div>
